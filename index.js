cad48eaefa153b0e68f1a42161ca96ac8b7c9d09
Shubh Shubhanjal <shubhshubhanjal96@gmail.com>
2024-02-16 18:55:51 +0530

    Added codes

--- index.js
+++ index.js
@@ -11,15 +11,24 @@ io.on('connection' , (socket)=> {
     console.log("A user connected" , socket.id);
     // "on" listens the event and "emit" publishes the event
    // This is for listening a event from a client side
-   socket.on('from_client' , ()=> {
-    console.log("Event coming from server")
-   })
+//    socket.on('from_client' , ()=> {
+//     console.log("Event coming from server")
+//    })
 
     // On website:- Go to inspect and then go to network (press ctrl+R) and then find for websockets in thise rows and click on it .Then click messages , there you find all the messages that is coming from the server.
     // This is for sending a event to client side
-    setInterval(()=>{
-        socket.emit('from_server')  // you can emit events on one side and register listeners on the other
-    } , 2000);
+    // setInterval(()=>{
+    //     socket.emit('from_server')  // you can emit events on one side and register listeners on the other
+    // } , 2000);
+
+
+    socket.on('msg_send' , (data) => {
+        console.log(data)
+        // Take the message from one person and emitting to different one using emit. 
+        io.emit('msg_rec' , data)
+       // socket.emit('msg_rec' , data) There is a slight difference between socket.emit and io.emit , in socket , msg send by person 1 can only see their message despite others are connecting to same port. But in io , everyone can see the msg send by anyone , who are connected to that port . To see by example , open chrome and edge and connect to same link and then see the differences.
+       // socket.broadcast.emit('msg_rec' , data) , in this reciever can't see their msg but the others can see
+    })
 })
 
 //Method to connect the express with static files(because html is a static file)
--- public/script.js
+++ public/script.js
@@ -1,18 +1,37 @@
 var socket = io();  //Intializing this to make the realtion between backend and frontend
 
 // This event is from cliet/frontend to server side
-let btn = document.getElementById('begin');
-btn.onclick = () => {
-    socket.emit('from_client');   //Sending a message to the server from client
-}
+// let btn = document.getElementById('begin');
+// btn.onclick = () => {
+//     socket.emit('from_client');   //Sending a message to the server from client
+// }
 
 
 
 //Registering for that emit event
 // This event is from server to client side or frontend
-socket.on('from_server' , ()=> {
-    console.log("Collected a new event from server")  // this line will display in inspect's console. 
-    const div = document.createElement('div');
-    div.innerText = 'New event from server';
-    document.body.appendChild(div);
+// socket.on('from_server' , ()=> {
+//     console.log("Collected a new event from server")  // this line will display in inspect's console. 
+//     const div = document.createElement('div');
+//     div.innerText = 'New event from server';
+//     document.body.appendChild(div);
+// })
+
+
+// ************************ Main Chat app Code begins from here ******************************** //
+
+let btn = document.getElementById('btn');
+let inputMsg = document.getElementById('newmsg');
+let msgList = document.getElementById('msglist');
+
+btn.onclick = function exec(){
+    socket.emit('msg_send' , {
+        msg: inputMsg.value    // We can also send objects in message
+    });
+}
+
+socket.on('msg_rec' , (data)=>{
+    let listenMsg = document.createElement('li');
+    listenMsg.innerText = data.msg;
+    msgList.appendChild(listenMsg)
 })
